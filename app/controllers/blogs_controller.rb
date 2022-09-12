8356908c9bcfc953148dc8e2fd4d0de5bc9fbec1
EC2 Default User <ec2-user@ip-172-31-42-211.ap-northeast-1.compute.internal>
2022-09-12 06:43:55 +0000

    Chapter 3 投稿機能を作ろう

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,8 +6,25 @@ class BlogsController < ApplicationController
   end
 
   def new
+    @blog = Blog.new  
+  end
+  
+  def create
+
+    # ---- ここからコードを書きましょう ---- #
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
+    # ---- ここまでのあいだにコードを書きましょう ---- #
+
   end
 
   def edit
+  end
+  
+  # ---- 以下を追加してください ---- #
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :category, :body)
   end
 end
